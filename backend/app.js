a23a00507c3b64bdacbe9cac58c0eeb21270f19b
Anup <anuppoudel60@gmail.com>
2022-05-14 14:16:20 +1000

    routes created

--- backend/app.js
+++ backend/app.js
@@ -6,4 +6,18 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
+//using middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//import routes
+
+const postRoute = require("./routes/post");
+const userRoute = require("./routes/user");
+
+//using routes
+app.use("/api/v1", postRoute);
+app.use("/api/v1", userRoute);
+
 module.exports = app;
--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,26 +1,30 @@
-const { default: mongoose } = require("mongoose");
 const mongoose = require("mongoose");
 
-const mongoose = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
   caption: String,
+
   image: {
     public_id: String,
     url: String,
   },
+
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+
   createdAt: {
     type: Date,
     default: Date.now,
   },
+
   likes: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
   ],
+
   comments: [
     {
       user: {
