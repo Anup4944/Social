a523c5b36011a56061d04b71539171c6bc9bd55f
Anup <anuppoudel60@gmail.com>
2022-05-15 01:01:46 +1000

    update profile and password done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -79,6 +79,7 @@ exports.login = async (req, res) => {
     });
   }
 };
+
 exports.logout = async (req, res) => {
   try {
     res
@@ -139,6 +140,68 @@ exports.followUser = async (req, res) => {
         message: "User followed",
       });
     }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.updatePassword = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("+password");
+
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide old and new password",
+      });
+    }
+
+    const isMatch = await user.matchPassword(oldPassword);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect old password",
+      });
+    }
+    user.password = newPassword;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+exports.updateProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    const { name, email } = req.body;
+
+    if (name) {
+      user.name = name;
+    }
+    if (email) {
+      user.email = email;
+    }
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Profile updated",
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const { register, login, followUser, logout } = require("../controllers/user");
+const {
+  register,
+  login,
+  followUser,
+  logout,
+  updatePassword,
+  updateProfile,
+} = require("../controllers/user");
 const { isAuth } = require("../middlewares/auth");
 
 const router = express.Router();
@@ -8,5 +15,7 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/follow/:id").get(isAuth, followUser);
+router.route("/update/password").put(isAuth, updatePassword);
+router.route("/update/profile").put(isAuth, updateProfile);
 
 module.exports = router;
