1efcf98bff0df02ffa347b6c29cff0d81f68994d
Anup <anuppoudel60@gmail.com>
2022-05-14 15:53:44 +1000

    middleware created

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
 const app = express();
+const cookieParser = require("cookie-parser");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV !== "production") {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //import routes
 
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { createPost } = require("../controllers/post");
+const { isAuth } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.route("/post/upload").post(createPost);
+router.route("/post/upload").post(isAuth, createPost);
 
 module.exports = router;
