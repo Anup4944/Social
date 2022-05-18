7e396763c0b9463f355940e46f1eeb139b66d9cd
Anup <anuppoudel60@gmail.com>
2022-05-18 23:27:22 +1000

    loading animation added

--- frontend/src/component/loader/Loader.jsx
+++ frontend/src/component/loader/Loader.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./loader.css";
 
 const Loader = () => {
-  return <div>Loading.........</div>;
+  return (
+    <div className="loadingPage">
+      <div className="loadingCircle"></div>
+    </div>
+  );
 };
 
 export default Loader;
