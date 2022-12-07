a4c971acf0ca63ae0c7afb077eb0419226aa4862
Anup <anuppoudel60@gmail.com>
2022-12-07 21:38:00 +1100

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // if (process.env.NODE_ENV !== "production") {
 // }
@@ -8,7 +9,7 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "config/config.env" });
 
 //using middleware
-
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
